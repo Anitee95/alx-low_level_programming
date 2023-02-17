823344b8b590b3796b606efe39d6bd7f3a19c4c9
Anitee.95 <anitemplet@gmail.com>
2023-02-16 16:41:15 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  *  main - prints i"Programming is like building a multilingual
  *  puzzle, followed by a newline.
@@ -10,5 +9,5 @@ int main(void)
 
 {
 	puts("\"Programming is like building a multilingual puzzle");
-        return (0);
+		return (0);
 }	
