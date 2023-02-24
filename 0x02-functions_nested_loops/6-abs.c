fabcf2998696959867e278d675533c19ef3d5c64
Anitee.95 <anitemplet@gmail.com>
2023-02-24 11:30:33 -0800

    _abs

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,7 +3,7 @@
 /**
  * _abs - Computes the absolute value of an integer.
  * @c: The number to be computed.
- * Return: Absolute value or 0
+ * Return: Absolute value or zero
  */
 
 int _abs(int c)
