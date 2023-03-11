078f72284d299b4aa7aefe7df9fa5e2c88cc31b9
Anitee.95 <anitemplet@gmail.com>
2023-03-10 17:39:42 -0800

    liball

--- 0x09-static_libraries/1-isdigit.c
+++ 0x09-static_libraries/1-isdigit.c
@@ -1,9 +1,11 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
+#include "main.h"
 
-int _strlen(char *s)
+/**
+ * _isdigit - checks for a digit (0 through 9)
+ * @c: int to be checked
+ * Return: 1 if c is a digit, 0 otherwise
+ */
+int _isdigit(int c)
 {
-	return strlen(s);
+	return (c >= '0' && c <= '9');
 }
--- 0x09-static_libraries/1-memcpy.c
+++ 0x09-static_libraries/1-memcpy.c
@@ -1,10 +1,18 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
 
+/**
+ * _memcpy - Entry point
+ * * @dest: input
+ * @src: input
+ * @n: input
+ * Return: Always 0 (Success)
+ */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	return memcpy(dest, src, n);
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
+
+	return (dest);
 }
