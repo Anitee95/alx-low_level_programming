5418cc3b41a81b16afad0291766bd04c22343f36
Anitee.95 <anitemplet@gmail.com>
2023-03-16 18:03:31 -0700

    mul

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
+#include <unistd.h>
 
 
 	int _putchar(char c);
@@ -13,4 +14,4 @@
 	int *array_range(int min, int max);
 	void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
-	#endif
+	#endif /*MAIN_H*/
